4751b315b09a79a8cb707f2b051b0632e05179f4
Scienceboy99 <67172409+Scienceboy999@users.noreply.github.com>
2020-11-30 15:08:19 +0200

    Added savedData

--- Lobby/Lobby.gd
+++ Lobby/Lobby.gd
@@ -5,8 +5,9 @@ func _ready() -> void:
 	if OS.has_feature("standalone"):
 		OS.window_fullscreen = true
 		OS.window_borderless = true
-	$ColorRect/VBoxContainer/IPEdit.text = Network.DEFAULT_IP
-	$ColorRect/VBoxContainer/PortEdit.text = str(Network.DEFAULT_PORT)
+	$ColorRect/VBoxContainer/NameEdit.text = Saved.saveData["playerName"]
+	$ColorRect/VBoxContainer/IPEdit.text = Saved.saveData["IP"]
+	$ColorRect/VBoxContainer/PortEdit.text = str(Saved.saveData["port"])
 
 func _on_HostButton_pressed() -> void:
 	Network.selected_IP = $ColorRect/VBoxContainer/IPEdit.text
@@ -22,3 +23,15 @@ func _on_JoinButton_pressed() -> void:
 
 func _on_SettingsButton_pressed() -> void:
 	$Settings.popup_centered()
+
+func _on_NameEdit_text_changed(new_text: String) -> void:
+	Saved.saveData["playerName"] = $ColorRect/VBoxContainer/NameEdit.text
+	Saved.saveGame()
+
+func _on_IPEdit_text_changed(new_text: String) -> void:
+	Saved.saveData["IP"] = $ColorRect/VBoxContainer/IPEdit.text
+	Saved.saveGame()
+
+func _on_PortEdit_text_changed(new_text: String) -> void:
+	Saved.saveData["port"] = $ColorRect/VBoxContainer/PortEdit.text
+	Saved.saveGame()
---
+++ Saved/Saved.gd
@@ -0,0 +1,31 @@
+extends Node
+
+var saveData:Dictionary = {}
+const SAVEGAME:String = "user://Savegame.json"
+const defaultDic:Dictionary = {"playerName":"Unnamed","IP":Network.DEFAULT_IP,"port":Network.DEFAULT_PORT}
+func _ready() -> void:
+	saveData = getData()
+
+func getData() -> Dictionary:
+	var file:File = File.new()
+	if !file.file_exists(SAVEGAME):
+		saveData = defaultDic
+		saveGame()
+	file.open(SAVEGAME,File.READ)
+	var content:String = file.get_as_text()
+	var data = parse_json(content)
+	saveData = data
+	file.close()
+	
+	for key in defaultDic.keys():
+		if !saveData.has(key):
+			saveData[key] = defaultDic[key]
+			saveGame()
+	
+	return data
+
+func saveGame() -> void:
+	var saveGame:File = File.new()
+	saveGame.open(SAVEGAME,File.WRITE)
+	saveGame.store_line(to_json(saveData))
+	saveGame.close()
